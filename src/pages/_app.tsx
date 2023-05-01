74a57c2f54e1879ddb609a82e9dad0ccece77f1d
Asad Dhorajiwala <asadrehandhorajiwala@gmail.com>
2023-05-01 15:13:32 +0530

    Project setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@myriad-chess/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>Myriad Chess</title>
+        <meta name='description' content='Play chess with a myriad of AI opponents' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default App
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,84 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useCallback, useEffect, useState } from 'react'
+import { Chessboard } from 'react-chessboard'
+import { Chess, PieceSymbol, Square } from 'chess.js'
+import { cloneDeep } from 'lodash'
 
-const inter = Inter({ subsets: ['latin'] })
+// Move object with only the necessary properties for game.move()
+interface ShortMove {
+  from: Square
+  to: Square
+  promotion?: PieceSymbol
+}
 
-export default function Home() {
+const Home = () => {
+  // game state
+  const [game, setGame] = useState(new Chess())
+  // whether it's the AI's turn
+  const [aiTurn, setAiTurn] = useState(false)
+
+  // make a move, if the move is illegal, return false
+  // if the move is legal, update the game state, set aiTurn to true and return true
+  const makeAMove = useCallback(
+    (move: ShortMove) => {
+      const gameCopy = cloneDeep(game)
+      try {
+        gameCopy.move(move)
+        setGame(gameCopy)
+        setAiTurn(true)
+        console.log(aiTurn ? 'ai:' : 'player:', gameCopy.history({ verbose: true }).slice(-1)[0])
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+    },
+    [aiTurn, game]
+  )
+
+  // make a random move from the list of possible moves
+  // set aiTurn to false after the move is made
+  const aiMove = useCallback(() => {
+    const possibleMoves = game.moves({ verbose: true })
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length)
+    makeAMove(possibleMoves[randomIndex])
+    setAiTurn(false)
+  }, [game, makeAMove])
+
+  // make the AI move if it's the AI's turn and the game is not over
+  useEffect(() => {
+    if (!aiTurn) return
+    if (game.isGameOver()) {
+      if (game.isCheckmate()) {
+        const winner = game.turn() === 'w' ? 'Black' : 'White'
+        console.log(`Checkmate! ${winner} wins!`)
+      } else if (game.isDraw()) {
+        console.log('Draw!')
+      } else {
+        console.log('Stalemate / 3-fold repetition!')
+      }
+      return
+    }
+    const interval = setInterval(() => aiMove(), 200)
+    return () => clearInterval(interval)
+  }, [game, aiMove, aiTurn])
+
+  // make the user's move when a piece is dropped
+  const onDrop = (sourceSquare: Square, targetSquare: Square) => {
+    return makeAMove({
+      from: sourceSquare,
+      to: targetSquare,
+      promotion: 'q' // always promote to a queen if move results in a pawn reaching the 8th rank
+    })
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Chessboard
+      position={game.fen()}
+      onPieceDrop={onDrop}
+      arePiecesDraggable={!game.isGameOver() && !aiTurn}
+      boardWidth={720}
+    />
   )
 }
+
+export default Home
